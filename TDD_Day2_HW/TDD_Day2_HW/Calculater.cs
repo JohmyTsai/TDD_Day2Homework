1990369f126c803377f69f8f82849d8fa25ab0c8
JohmyTsai <adia3141@gmail.com>
2016-07-10 02:36:28 +0800

    第二個TestCase Pass

--- TDD_Day2_HW/TDD_Day2_HW/Calculater.cs
+++ TDD_Day2_HW/TDD_Day2_HW/Calculater.cs
@@ -6,34 +6,59 @@ namespace TDD_Day2_HW
     {
         internal int Sum(Potter order)
         {
-            int SetSum = 0;
+            int Total = 0;
+            int SetType = 0;
 
             while (order.First != 0 || order.Second != 0 || order.Third != 0 || order.Fourth != 0 || order.Fifth != 0)
             {
                 if (order.First != 0)
                 {
                     order.First--;
+                    SetType++;
                 }
                 if (order.Second != 0)
                 {
                     order.Second--;
+                    SetType++;
                 }
                 if (order.Third != 0)
                 {
                     order.Third--;
+                    SetType++;
                 }
                 if (order.Fourth != 0)
                 {
                     order.Fourth--;
+                    SetType++;
                 }
                 if (order.Fifth != 0)
                 {
                     order.Fifth--;
+                    SetType++;
                 }
 
-                SetSum++;
+                Total += GetSetSum(SetType);
+                SetType++;
             }
-            return SetSum * 100;
+
+            return Total;
+        }
+
+        private int GetSetSum(int setType)
+        {
+            switch (setType)
+            {
+                case 1:
+                    // Set1 100 * 1 = 100
+                    return 100;
+
+                case 2:
+                    // Set2 100 * 2 * 0.95 = 190
+                    return 190;
+
+                default:
+                    return 0;
+            }
         }
     }
 }
