a6c4e637509ffc98692c8ae28b46c2ee89a2d6d7
JohmyTsai <adia3141@gmail.com>
2016-07-10 02:39:33 +0800

    建立第三個 TestCase

--- TDD_Day2_HW/TDD_Day2_HW/UnitTest1.cs
+++ TDD_Day2_HW/TDD_Day2_HW/UnitTest1.cs
@@ -49,5 +49,27 @@ namespace TDD_Day2_HW
             //Assert
             Assert.AreEqual(expect, actual);
         }
+
+        [TestMethod]
+        public void Buy_1_The_First_And_Second_And_Third_Potter()
+        {
+            //Arrage
+            var order = new Potter
+            {
+                First = 1,
+                Second = 1,
+                Third = 1,
+                Fourth = 0,
+                Fifth = 0
+            };
+            int expect = 270;
+
+            //Act
+            Calculater target = new Calculater();
+            int actual = target.Sum(order);
+
+            //Assert
+            Assert.AreEqual(expect, actual);
+        }
     }
 }
