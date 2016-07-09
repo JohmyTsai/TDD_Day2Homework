e3eb97312a9ed9755734a58ab5c36d5f9c7d14c0
JohmyTsai <adia3141@gmail.com>
2016-07-09 23:48:59 +0800

    init project

---
+++ TDD_Day2_HW/TDD_Day2_HW/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TDD_Day2_HW
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Buy_1_the_first_Potter()
+        {
+            //Arrage
+            var order = new Potter
+            {
+                First = 0,
+                Second = 0,
+                Third = 0,
+                Fourth = 0,
+                Fifth = 0
+            };
+            int expect = 100;
+
+            //Act
+            Calculater target = new Calculater();
+            int actual = target.Sum(order);
+
+            //Assert
+            Assert.AreEqual(expect, actual);
+        }
+    }
+}
